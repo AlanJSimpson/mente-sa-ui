0e44a690c7f0748684ab52f54ea52cf4457babbd
AlanJSimpson <allanstark2908@gmail.com>
2022-08-29 12:11:14 -0300

    Dashboard intializado de forma estática.

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -1,9 +1,44 @@
+import { Box, Card, CardContent } from '@mui/material';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import './dashboardPage.css';
 
 export default function DashboardPage() {
   return (
     <Container>
-      <h1>Página do DashBoard</h1>
+      <Typography sx={{ marginY: '2rem' }} component='h2' variant='h3'>
+        DashBoard
+      </Typography>
+
+      <Box className='card-box'>
+        {[
+          { title: 'Sessões agendadas (dia)', value: 5 },
+          { title: 'Sessões agendadas (mês)', value: 15 },
+          { title: 'Sessões canceladas', value: 1 },
+          { title: 'Total de pacientes cadastrados', value: 546 },
+          { title: 'Total de sessões (individuais)', value: 10 },
+        ].map((card, index) => {
+          return (
+            <Card
+              key={index}
+              sx={{ width: 200, marginX: 5, marginBottom: '2rem' }}
+            >
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color='text.secondary'
+                  gutterBottom
+                >
+                  {card.title}
+                </Typography>
+                <Typography textAlign='center' variant='h5' component='div'>
+                  {card.value}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Box>
     </Container>
   );
 }
--- src/pages/pacientes/PacientePage.tsx
+++ src/pages/pacientes/PacientePage.tsx
@@ -1,9 +1,66 @@
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Button,
+  InputAdornment,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import './pacientePage.css';
 
 export default function PacientePage() {
   return (
     <Container>
-      <h1>Página do paciente</h1>
+      <Box className='paciente-section'>
+        <Typography component='h2' variant='h4'>
+          Meus Pacientes
+        </Typography>
+        <TextField
+          sx={{ width: '50%' }}
+          variant='standard'
+          placeholder='Pesquisar'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button startIcon={<AddCircleIcon />} variant='contained'>
+          Novo Paciente
+        </Button>
+      </Box>
+
+      <TableContainer sx={{ marginTop: '2rem' }} component={Paper}>
+        <Table
+          sx={{ minWidth: 650 }}
+          size='small'
+          aria-label='Tabela de pacientes'
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Endereço</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Gênero</TableCell>
+              <TableCell>Cidade</TableCell>
+              <TableCell>Município</TableCell>
+              <TableCell>Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody></TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 }
